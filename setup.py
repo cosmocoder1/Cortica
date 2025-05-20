30ea7223c648f214d93c39aa5ad589582de3ed16
Nathan Lucy <cosmocoder1@gmail.com>
2025-05-20 12:38:20 -0500

    added setup file for module import

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cortica",
+    version="0.1.0",
+    description="A lightweight cognitive memory engine for semantic storage and retrieval.",
+    packages=find_packages(),
+    install_requires=[],
+    author="Nathan A. Lucy",
+    python_requires=">=3.10",
+)
